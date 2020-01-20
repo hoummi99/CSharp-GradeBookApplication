0b3e5d966f723c2c22e30e864421242b703d000d
Ismail Hoummi <ismailhoummi@tavil.net>
2020-01-20 20:46:01 +0100

    Update BaseGradeBook.cs

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -110,7 +110,7 @@ namespace GradeBook.GradeBooks
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
             int weight = 0;
-            if (IsWeighted)
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
                 weight = 1;
 
             switch (letterGrade)
